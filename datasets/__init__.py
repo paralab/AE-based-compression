9c81c410a77ba76842850a4128de0e1f65a41898
Tahmid Awal <tawal@dt-login04.delta.ncsa.illinois.edu>
2025-07-09 12:14:54 -0500

    pushing changes for cleaned code

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,6 +1,7 @@
 from .datasets import register, make
-from . import image_folder
-from . import math_function
-from . import math_function_3d
-from . import wrappers
-from . import swae_3d_dataset
+# from . import image_folder
+# from . import math_function
+# from . import math_function_3d
+# from . import wrappers
+# from . import swae_3d_dataset
+from . import u_chi_dataset
--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,11 @@
 from .models import register, make
-from . import edsr, rdn, rcan
-from . import mlp
-from . import thera
-from . import thera_3d
-from . import swae
-from . import swae_pure_3d
-from . import liif
-from . import liif_3d
-from . import misc
+# from . import edsr, rdn, rcan
+# from . import mlp
+# from . import thera
+# from . import thera_3d
+# from . import swae
+# from . import swae_pure_3d
+# from . import liif
+# from . import liif_3d
+# from . import misc
+from . import swae_pure_3d_7x7x7
